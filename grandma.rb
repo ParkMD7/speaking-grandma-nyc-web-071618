c4435582e6eb42cafc8b50c654d43ca053777020
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-11 16:33:05 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(string)
-  if string = string.downcase()
+  if string == string.downcase()
     return "HUH?! SPEAK UP, SONNY!"
     
   elsif string = "I LOVE YOU GRANDMA!"
