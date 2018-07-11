881725aa75f22378fe1980bd2680896144dd65ef
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-11 16:28:44 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,5 +17,9 @@ def speak_to_grandma(string)
     return "HUH?! SPEAK UP, SONNY!"
   elsif string = string.upcase()
     return "NO, NOT SINCE 1938!"
-  elsif string = ""
+  elsif string = "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  else
+    return nil
+  end
+end
